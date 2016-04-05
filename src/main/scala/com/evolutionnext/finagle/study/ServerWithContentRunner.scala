d121719f1c2cfabab82f78b0243437fbc56be5b8
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-04-05 13:41:48 -0600

    Updated Filter Example

--- src/main/scala/com/evolutionnext/finagle/study/ServerWithContentRunner.scala
+++ src/main/scala/com/evolutionnext/finagle/study/ServerWithContentRunner.scala
@@ -1,5 +1,20 @@
 package com.evolutionnext.finagle.study
 
-object ServerWithContentRunner {
+import java.io.File
 
+import com.twitter.finagle.{Http, Service, http}
+import com.twitter.io.Reader
+import com.twitter.util.{Await, Future}
+
+object ServerWithContentRunner extends App {
+  val service = new Service[http.Request, http.Response] {
+    def apply(req: http.Request): Future[http.Response] =
+      Future.value(
+        http.Response(req.version, http.Status.Ok,
+          Reader.fromFile(new File("/home/danno/.zshrc")))
+      )
+  }
+
+  val server = Http.serve(":10350", service)
+  Await.ready(server)
 }
--- src/test/scala/com/evolutionnext/finagle/study/filters/FiltersTest.scala
+++ src/test/scala/com/evolutionnext/finagle/study/filters/FiltersTest.scala
@@ -1,12 +1,43 @@
 package com.evolutionnext.finagle.study.filters
 
-import com.twitter.finagle.http.{Request, Response}
-import com.twitter.finagle.{Http, Service, SimpleFilter, http}
-import com.twitter.util.{Await, Future}
+import com.twitter.finagle.Http
+import com.twitter.finagle.http.{Method, Request, Response}
+import com.twitter.util.Future
 import org.scalatest.FunSuite
 
 class FiltersTest extends FunSuite {
-  test("Running with a future") {
 
+  test("Running a request of a private resource") {
+    val httpService = Http.newService("localhost:10350")
+
+    val future: Future[Response] = httpService.apply(
+      Request(Method.Get, "/private/panama.html"))
+
+    future onSuccess { response: Response =>
+      println("received response status: " + response.status +
+        " with content:" + response.getContentString())
+    }
+
+    future onFailure { throwable: Throwable =>
+      println("received failure: " + throwable.getMessage)
+    }
+    Thread sleep 10000
+  }
+
+  test("Running a request of a public resource") {
+    val httpService = Http.newService("localhost:10350")
+
+    val future: Future[Response] = httpService.apply(
+      Request(Method.Get, "/public/open_knowledge.html"))
+
+    future onSuccess { response: Response =>
+      println("received response status: " + response.status +
+        " with content:" + response.getContentString())
+    }
+
+    future onFailure { throwable: Throwable =>
+      println("received failure: " + throwable.getMessage)
+    }
+    Thread sleep 10000
   }
 }
